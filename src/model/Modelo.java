f274c4c0d3ad88118e71c55a372422f0b776bc30
Alejandro Slabkov <aslabkov@entaconsulting.com>
2016-05-27 18:48:29 -0300

    cambios en modelo

--- src/model/Modelo.java
+++ src/model/Modelo.java
@@ -1,5 +1,40 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Modelo {
+	
+	private String nombre;
+	private Integer cantidadPuertas;
+	private LocalDate fechaLanzamiento;
+	private float precioBase;
+	
+	public Modelo(String nombre, Integer cantidadPuertas, LocalDate fechaLanzamiento, float precioBase){
+		this.nombre = nombre;
+		this.cantidadPuertas = cantidadPuertas;
+		this.fechaLanzamiento = fechaLanzamiento;
+		this.precioBase = precioBase;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public Integer getCantidadPuertas(){
+		return cantidadPuertas;
+	}
+	
+	public LocalDate getFechaLanzamiento(){
+		return fechaLanzamiento;
+	}
+	
+	public float getPrecioBase(){
+		return precioBase;
+	}
+	
+	public void setPrecio(float precio){
+		this.precioBase = precio;
+	}
+	
 
 }
