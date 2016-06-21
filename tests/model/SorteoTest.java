8b378f0028c805f81acb44263c953d9c6165ab3b
Alejandro Slabkov <aslabkov@entaconsulting.com>
2016-06-20 21:57:12 -0300

    Sorteo Test Change

--- tests/model/SorteoTest.java
+++ tests/model/SorteoTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class SorteoTest {
@@ -19,10 +20,7 @@ public class SorteoTest {
 	public void setUp() throws Exception {
 		
 		this.rndMock = mock(Random.class);
-		
-		//when(mockMensaje.getCreador()).thenReturn(mockUser);
-		//when(mockMensaje.getDescripcion()).thenReturn("Este es el mensaje");
-		
+
 		sorteo = new Sorteo(rndMock);
 	}
 
@@ -31,16 +29,20 @@ public class SorteoTest {
 		
 		PlanDeAhorro p = mock(PlanDeAhorro.class);
 		
-		ArrayList<Subscripcion> arr = new ArrayList<Subscripcion>();
+		Subscripcion subscripcion = mock(Subscripcion.class);
 		
-		arr.add(new Subscripcion(mock(Cliente.class)));
-		arr.add(new Subscripcion(mock(Cliente.class)));
-		arr.add(new Subscripcion(mock(Cliente.class)));
+		List<Subscripcion> subscripcionesSinAdjudicacion = mock(List.class);
+		when(subscripcionesSinAdjudicacion.get(1)).thenReturn(subscripcion);
+		when(subscripcionesSinAdjudicacion.size()).thenReturn(3);
 		
-		when(p.getSubscripcionesSinAdjudicacion()).thenReturn(arr);
 		
-		assertNotNull(sorteo.adjudicar(p));
 		
+		when(p.getSubscripcionesSinAdjudicacion()).thenReturn(subscripcionesSinAdjudicacion);
+		when(rndMock.nextInt(3)).thenReturn(1);
+		
+		assertEquals(sorteo.adjudicar(p), subscripcion);
+		
+		verify(subscripcion).registrarAdjudicacion(any());
 		verify(p).getSubscripcionesSinAdjudicacion();
 		verify(rndMock).nextInt(3);
 		
